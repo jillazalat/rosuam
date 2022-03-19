920555370c212fba54b05b1099d631d3de7a7ce6
Jill Aghyourli Zalat <39888814+jaghz@users.noreply.github.com>
2022-03-19 14:09:35 -0400

    Update object_tracking.py

--- jimprocessing/src/object_tracking.py
+++ jimprocessing/src/object_tracking.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# Refs https://github.com/trunc8/ball-tracking-opencv-and-ros
+# https://www.instructables.com/Automatic-Vision-Object-Tracking/
 from __future__ import print_function
 
 import roslib
@@ -10,12 +12,13 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import CompressedImage
 import numpy as np
-
+import imutils
 
 class image_converter:
 
     def __init__(self):
         self.image_pub = rospy.Publisher("image_topic_2", Image)
+        #self.dist_pub = rospy.Publisher("z_dist_to_camera", String)
 
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber(
@@ -25,53 +28,51 @@ class image_converter:
     def callback(self, data):
         try:
             cv_image = self.bridge.compressed_imgmsg_to_cv2(data, "bgr8")
+            cv_image = cv2.flip(cv_image, 0)
         except CvBridgeError as e:
             print(e)
-
-           # Image manipulation stuff
-           # -------------------------------------------
-        # (rows,cols,channels) = cv_image.shape
-        # if cols > 60 and rows > 60 :
-        # cv2.circle(cv_image, (50,50), 10, 255)
-
-        ## Instructables Step 4 - convert BGR to HSV ##
-        # color vals found from ms paint eyedropper tool
-        blue = 210
-        green = 159
-        red = 102
-        color = np.uint8([[[blue, green, red]]])
-        hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-        frame = cv_image
-        hue = hsv[0][0][0]
-        colorLower = (94, 100, 100)
-        colorUpper = (114, 255, 255)
-
-        # --------------------------------------------
         mask_img = self.get_masked_image(cv_image)
+        rectangle=None
+        imwidth = None
+        box_width = 4.5 # cm
+        focal_length = 360 
         contours, hierarchy = cv2.findContours(mask_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         center = None
+        if len(contours) <= 0:
+            print("Object not detected\n")
         if len(contours) > 0:
             c = max(contours, key=cv2.contourArea)
-            ((x,y), radius) = cv2.minEnclosingCircle(c)
-            M= cv2.moments(c)
-            center=(int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            ((xc,yc), radius) = cv2.minEnclosingCircle(c)
             if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-                cv2.circle(cv_image, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(cv_image, center, 5, (0, 0, 255), -1)
-        #print(x,y)
+                rectangle = cv2.minAreaRect(c)
+                x,y,w,h = cv2.boundingRect(c)
+                cv2.rectangle(cv_image,(x,y),(x+w,y+h),(0,255,0),2)
+                centerx, centery = x+(w//2), y+(h//2)
+                center = (centerx, centery)
+                cv2.circle(cv_image, center, 3, (0, 0, 255), -1)
+
+                imwidth = abs(rectangle[1][0])#px
+                area=round(rectangle[1][0]*rectangle[1][1])
+                #rel_focal_length = imwidth*20/4.5 #cm
+                #print(rel_focal_length)
+                dist = round((box_width*focal_length/rectangle[1][0])*0.8,1)
+                coords = (x,y,dist)
+                text = str(coords) + " A = " + str(area)#'Distance from camera in cm:'+
+                cv2.putText(cv_image, text,(30,20),cv2.FONT_HERSHEY_SIMPLEX, .3,(0,0,255),1,cv2.LINE_AA)
+                #actual_distance_to_obj = str(round(7+(focal_length*actual_width/imwidth),2)) + " cm"
+                #print("x-y coordinates:",center)
 	# Show video stream of blue box with circle around it
-        imS = cv2.resize(cv_image, (250, 250))  		     
+        imS = cv2.resize(cv_image, (400, 400))  		     
         cv2.imshow("blue box live feed", imS)
-        #cv2.imshow("Black Background Tracking", black_img)
+
         cv2.waitKey(3)
         # Show video stream of just the mask
-        imS = cv2.resize(mask_img, (250, 250))
-        cv2.imshow("mask stream", imS)
-        cv2.waitKey(3)
+        #imS = cv2.resize(mask_img, (250, 250))
+        #cv2.imshow("mask stream", imS)
+        #cv2.waitKey(3)
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+            
         except CvBridgeError as e:
             print(e)
         
@@ -82,7 +83,6 @@ class image_converter:
         colorUpper = (114, 255, 255)
         mask = cv2.inRange(hsv, colorLower, colorUpper)
         return mask
-
 
     
 def main(args):
