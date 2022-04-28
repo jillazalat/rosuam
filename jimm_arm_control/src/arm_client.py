a86d029669dd0dc0b553d4ff9e15e7c431df7178
Matthew <yafta98@gmail.com>
2022-04-27 20:10:26 -0400

    Update arm client

--- jimm_arm_control/src/arm_client.py
+++ jimm_arm_control/src/arm_client.py
@@ -11,25 +11,27 @@ kit = ServoKit(channels=16)
 
 def gripper_callback(data):
     grip_target_angle = data.data
-#    rospy.loginfo("grip angle: "+ str(data.data))
+    rospy.loginfo("gripper angle: "+ str(data.data))
     kit.servo[0].angle=grip_target_angle
+    
 def elbow_callback(data):
     elbow_angle = data.data
     rospy.loginfo("elbow angle: " + str(data.data))
-    kit.servo[1].angle=elbow_angle
-#    time.sleep(0.2)
+    kit.servo[1].angle = elbow_angle
+
 def approach_callback(data):
     approach_angle = data.data
-    kit.servo[2].angle=approach_angle
+    rospy.loginfo("approach angle: " + str(data.data))
+    kit.servo[2].angle = approach_angle
     
 def listener():
     rospy.init_node('arm_client', anonymous=True)
-    rospy.Subscriber('gripper_angle', Int32, gripper_callback)
-    rospy.Subscriber('tilt_angle', Int32, elbow_callback)
-    rospy.Subscriber('approach_angle',Int32,approach_callback)
-    kit.servo[0].angle = 0
-    kit.servo[1].angle = 35
-    kit.servo[2].angle = 130
+    rospy.Subscriber('/rosuam/gripper_angle', Int32, gripper_callback)
+    rospy.Subscriber('/rosuam/tilt_angle', Int32, elbow_callback)
+    rospy.Subscriber('/rosuam/approach_angle',Int32,approach_callback)
+    #kit.servo[0].angle = 0
+    #kit.servo[1].angle = 35
+    #kit.servo[2].angle = 130
     rospy.spin()
 
 
